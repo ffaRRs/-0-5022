b8076000aa48a715c45887987fc17a348c3669af
ffaRRs <71458239+ffaRRs@users.noreply.github.com>
2020-09-17 21:43:58 +0300

    Добавьте файлы проекта.

---
+++ Main.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+main()
+{
+	int a, b, c, res;
+	printf("vvedite a:");
+	scanf("%d", &a);
+	printf("vvedite b:");
+	scanf("%d", &b);
+	printf("vvedite c:");
+	scanf("%d", &c);
+	res = a * c;
+	printf("res = %d \n", res);
+	return 0;
+
+
+
+
+}
